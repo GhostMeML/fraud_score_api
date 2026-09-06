4a57cf2176f162e8d70f133d03d929009a176658
Valeria <151046367+GhostMeML@users.noreply.github.com>
2026-09-06 22:14:13 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./fraud_api.db"
 
-# connect_args={"check_same_thread": False} нужен только для SQLite в FastAPI
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
@@ -11,7 +11,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Зависимость для получения сессии БД (Dependency Injection)
+
 def get_db():
     db = SessionLocal()
     try:
